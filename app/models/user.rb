9bbbe50c6773625fa47879229e166a068883c5bd
Ellen Cornelius <ecorneli@iwu.edu>
2018-04-05 10:31:11 -0600

    [#156520058] Generate user model and migration.

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+
+  def self.update_or_create(auth)
+    user = User.find_by(uid: auth[:uid]) || User.new
+    user.attributes = {
+    provider: auth[:provider],
+    uid: auth[:uid],
+    email: auth[:info][:email],
+    first_name: auth[:info][:first_name],
+    last_name: auth[:info][:last_name],
+    token: auth[:credentials][:token]
+  }
+  user.save!
+  user
+  end
+end
