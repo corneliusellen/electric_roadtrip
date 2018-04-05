8ec4d5ee65f225738b2c7ea3d8e6d803affcdb5e
Ellen Cornelius <ecorneli@iwu.edu>
2018-04-05 16:45:30 -0600

    [#156527795] Create spec for user visiting dashboard.

---
+++ spec/features/user_visits_dashboard_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe "user visits dashboard" do
+  context "user has logged in" do
+
+    let(:user) { User.new }
+
+    it "user sees name and button to add new vehicles" do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      expect(user).to receive(:first_name).and_return("Ellen")
+      expect(user).to receive(:last_name).and_return("Corn")
+      visit dashboard_path
+
+      expect(page).to have_content("Hello, Ellen Corn")
+      expect(page).to have_link("Add First Vehicle")
+      expect(page).to have_link("Logout")
+    end
+  end
+end
