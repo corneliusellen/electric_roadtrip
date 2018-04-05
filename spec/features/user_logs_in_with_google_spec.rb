6c0cdefb259aa84f207d8371ae60ee0b5d640067
Ellen Cornelius <ecorneli@iwu.edu>
2018-04-05 10:16:45 -0600

    Create test for google oauth.

---
+++ spec/features/user_logs_in_with_google_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+def stub_omniauth
+  OmniAuth.config.test_mode = true
+  OmniAuth.config.mock_auth[:google] = OmniAuth::AuthHash.new({
+    provider: "google",
+      uid: "123456789",
+      info: {
+        email: "gellinellen@gmai.com",
+        first_name: "Ellen",
+        last_name: "Cornelius"
+      },
+      credentials: {
+        token: "abc123",
+        refresh_token: "123ab",
+        expires_at: DateTime.now,
+      }
+    })
+end
+
+describe "user logs in" do
+  scenario "using google oauth2" do
+    stub_omniauth
+    visit root_path
+    click_link("Sign in with Google")
+
+    expect(page).to have_content("Hello, Ellen Cornelius")
+    expect(page).to have_link("Logout")
+  end
+end
