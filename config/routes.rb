6e60bcaf2f13da106ebba4a99da2e4b4d3977b02
Ellen Cornelius <ecorneli@iwu.edu>
2018-04-09 14:33:59 -0600

    Create comparision route and controller.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'logout', to: 'sessions#destroy'
   get 'dashboard', to: 'dashboard#index'
-  get '/compare', to: 'compare#new', as: 'compare_vehicles'
+  get '/comparision', to: 'comparision#new', as: 'new_comparision'
   resources :vehicles
 end
