b6ee3e10181441033008a70a35fb1718f3ac64d2
Ellen Cornelius <ecorneli@iwu.edu>
2018-04-12 15:05:03 -0600

    Add lat and long params and geocoding service instantiation.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,23 +4,11 @@ class SearchController < ApplicationController
   end
 
   def index
-    vehicle_info = VehicleSearch.new(params["Vehicle ID"]).find_vehicle
+    result = GeocodeService.new(params[:address])
+    result.geocode
+    @lat = result.lat
+    @long = result.long
+    vehicle_info = VehicleSearch.new(params[:vehicle_id]).find_vehicle
     @vehicle = Vehicle.new(vehicle_info)
   end
-end
-
-class VehicleSearch
-  def initialize(id)
-    @id = id.to_i
-  end
-
-  def find_vehicle
-      response = Faraday.get("https://www.fueleconomy.gov/ws/rest/vehicle/#{id}")
-      json = Hash.from_xml(response.body).to_json
-      JSON.parse(json)["vehicle"]
-  end
-
-  private
-
-  attr_reader :id
 end
