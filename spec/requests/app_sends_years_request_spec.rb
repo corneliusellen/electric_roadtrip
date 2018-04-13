2d9ff3ab10f2754b59f10869d3a95b55034b8b0c
Ellen Cornelius <ecorneli@iwu.edu>
2018-04-13 00:42:22 -0600

    Create specs for endpoints.

---
+++ spec/requests/app_sends_years_request_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe "Years API" do
+  it "sends years in json format" do
+    get "/api/v1/years"
+
+    results = JSON.parse(response.body)
+
+    expect(response.body).to be_a String
+    expect(results.count).to eq(36)
+  end
+end
