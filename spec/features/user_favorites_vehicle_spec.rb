924f5baa84f13386bcb585d676e909cfc871d3e6
Ellen Cornelius <ecorneli@iwu.edu>
2018-04-16 19:05:11 -0600

    Add selenium methods to feature tests.

--- spec/features/user_favorites_vehicle_spec.rb
+++ spec/features/user_favorites_vehicle_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
-describe "User can add vehicle to profile" do
+describe "User can add vehicle to profile", js: true do
+  before(:all) do
+    Capybara.current_driver = :selenium
+  end
   context "As a user" do
 
     let(:user) { User.new }
@@ -10,11 +13,23 @@ describe "User can add vehicle to profile" do
       visit new_search_path
 
       fill_in "Enter Home Address", with: "1331 17th St, Denver, CO"
-      within '.year-menu' do
-        find("option[value='2018']").select_option
+      driver = page.driver.browser
+      options = driver.find_element(:class, 'year-menu').find_elements(tag_name: 'option')
+      options.each do |option|
+        option.click if option.attribute('value') == "2018"
       end
-      select "Chevrolet", from: "make-menu"
-      select "Volt", from: "Select Model"
+
+      driver = page.driver.browser
+      options = driver.find_element(:class, 'make-menu').find_elements(tag_name: 'option')
+      options.each do |option|
+        option.click if option.attribute('value') == "Chevrolet"
+      end
+
+      driver = page.driver.browser
+      dropdown = driver.find_element(:class, 'option-menu')
+      options = dropdown.find_elements(tag_name: 'option')
+      options.each { |option| option.click if option.text == "Auto (variable gear ratios), 4 cyl, 1.5 L"}
+
       click_on 'Find'
 
       expect(current_path).to eq(new_search_path)
--- spec/features/user_searches_for_vehicle_spec.rb
+++ spec/features/user_searches_for_vehicle_spec.rb
@@ -1,36 +1,36 @@
-# require 'rails_helper'
-#
-# describe "User can add vehicle to profile" do
-#   context "As a user" do
-#
-#     let(:user) { User.new }
-#
-#     it "I can add a vehicle to my profile page" do
-#       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-#       visit dashboard_path
-#       click_on "Add First Vehicle"
-#
-#       expect(current_path).to eq(new_search_path)
-#       expect(page).to have_content("Find Your Vehicle")
-#
-#       fill_in "Enter Home Address", with: "1331 17th St, Denver, CO"
-#
-#       within '.year-menu' do
-#         find("option[value='2018']").select_option
-#       end
-#       select "Chevrolet", from: "make-menu"
-#       select "Volt", from: "Select Model"
-#       click_on 'Find'
-#
-#       expect(current_path).to eq(search_path)
-#       expect(page).to have_content("Make: Chevrolet")
-#       expect(page).to have_content("Model: Volt")
-#       expect(page).to have_content("Year: 2014")
-#       expect(page).to have_content("Fuel Type: Premium Gas or Electricity")
-#       expect(page).to have_content("Range: 38 miles")
-#       expect(page).to have_content("MPGe: 98")
-#       expect(page).to have_content("Time to charge at 120V: n/a")
-#       expect(page).to have_content("Time to charge at 240V: 4.0 hours")
-#     end
-#   end
-# end
+require 'rails_helper'
+
+describe "User can add vehicle to profile" do
+  context "As a user" do
+
+    let(:user) { User.new }
+
+    it "I can add a vehicle to my profile page" do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      visit dashboard_path
+      click_on "Add First Vehicle"
+
+      expect(current_path).to eq(new_search_path)
+      expect(page).to have_content("Find Your Vehicle")
+
+      fill_in "Enter Home Address", with: "1331 17th St, Denver, CO"
+
+      within '.year-menu' do
+        find("option[value='2018']").select_option
+      end
+      select "Chevrolet", from: "make-menu"
+      select "Volt", from: "Select Model"
+      click_on 'Find'
+
+      expect(current_path).to eq(search_path)
+      expect(page).to have_content("Make: Chevrolet")
+      expect(page).to have_content("Model: Volt")
+      expect(page).to have_content("Year: 2014")
+      expect(page).to have_content("Fuel Type: Premium Gas or Electricity")
+      expect(page).to have_content("Range: 38 miles")
+      expect(page).to have_content("MPGe: 98")
+      expect(page).to have_content("Time to charge at 120V: n/a")
+      expect(page).to have_content("Time to charge at 240V: 4.0 hours")
+    end
+  end
+end
