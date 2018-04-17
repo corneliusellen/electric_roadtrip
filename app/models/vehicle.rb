ad1babe6612574159a48ca43e654a9360d6baebd
Ellen Cornelius <ecorneli@iwu.edu>
2018-04-16 19:07:00 -0600

    Add active record to models.

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,21 +1,5 @@
-class Vehicle
-  attr_reader :make,
-              :model,
-              :year,
-              :fuel_type,
-              :range,
-              :mpge
-
-  def initialize(vehicle_info)
-    @make = vehicle_info["make"]
-    @model = vehicle_info["model"]
-    @year = vehicle_info["year"]
-    @fuel_type = vehicle_info["fuelType"]
-    @range = vehicle_info["rangeA"]
-    @mpge = vehicle_info["combA08"]
-    @charge120 = vehicle_info["charge120"]
-    @charge240 = vehicle_info["charge240"]
-  end
+class Vehicle < ApplicationRecord
+  belongs_to :user
 
   def charge_120
     if charge120 == "0.0"
@@ -33,8 +17,4 @@ class Vehicle
     end
   end
 
-  private
-
-  attr_reader :charge120,
-              :charge240
 end
