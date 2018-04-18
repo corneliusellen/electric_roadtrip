f45d10a57e200950ed8848209dadcb6f27ca255c
Ellen Cornelius <ecorneli@iwu.edu>
2018-04-18 14:15:48 -0600

    Remove pry.

--- spec/services/nrel_service_spec.rb
+++ spec/services/nrel_service_spec.rb
@@ -4,6 +4,5 @@ describe NrelService do
   it "does the thing" do
     linestring = GeocodeService.new.get_linestring(1, 2)
     stations = NrelService.new(linestring, 2).get_stations
-    binding.pry
   end
 end
