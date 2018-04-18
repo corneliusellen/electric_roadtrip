79c66b3fdab5c96323ccabc43910ecaca2794663
Ellen Cornelius <ecorneli@iwu.edu>
2018-04-17 20:19:26 -0600

    Update specs with correct Selenium methods.

--- spec/features/user_favorites_vehicle_spec.rb
+++ spec/features/user_favorites_vehicle_spec.rb
@@ -1,45 +1,44 @@
 require 'rails_helper'
 
-describe "User can add vehicle to profile", js: true do
+describe "User can add vehicle to profile" do
+
   before(:all) do
     Capybara.current_driver = :selenium
   end
+
   context "As a user" do
 
-    let(:user) { User.new }
+    let(:user) { create(:user) }
 
     it "I can add a vehicle to my profile page" do
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
       visit new_search_path
 
       fill_in "Enter Home Address", with: "1331 17th St, Denver, CO"
-      driver = page.driver.browser
-      options = driver.find_element(:class, 'year-menu').find_elements(tag_name: 'option')
-      options.each do |option|
-        option.click if option.attribute('value') == "2018"
+
+      within '.year-menu' do
+        find("option[value='2018']").select_option
       end
-
-      driver = page.driver.browser
-      options = driver.find_element(:class, 'make-menu').find_elements(tag_name: 'option')
-      options.each do |option|
-        option.click if option.attribute('value') == "Chevrolet"
+      within '.make-menu' do
+        find("option[value='Chevrolet']").select_option
       end
-
-      driver = page.driver.browser
-      dropdown = driver.find_element(:class, 'option-menu')
-      options = dropdown.find_elements(tag_name: 'option')
-      options.each { |option| option.click if option.text == "Auto (variable gear ratios), 4 cyl, 1.5 L"}
-
-      click_on 'Find'
+      within '.model-menu' do
+        find("option[value='Bolt EV']").select_option
+      end
+      within '.option-menu' do
+        find("option[value='39786'").select_option
+      end
+      click_on 'Find!'
 
       expect(current_path).to eq(new_search_path)
 
       click_on "Save Vehicle"
 
       expect(current_path).to eq(dashboard_path)
-      expect(page).to have_content("Make: Chevrolet")
-      expect(page).to have_content("Model: Volt")
-      expect(page).to have_content("Year: 2014")
+      expect(page).to have_content("Vehicle saved")
+      expect(page).to have_content("Chevrolet")
+      expect(page).to have_content("Bolt EV")
+      expect(page).to have_content("2018")
     end
   end
 end
--- spec/features/user_searches_for_vehicle_spec.rb
+++ spec/features/user_searches_for_vehicle_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 describe "User can add vehicle to profile" do
+
+  before(:all) do
+    Capybara.current_driver = :selenium
+  end
+
   context "As a user" do
 
     let(:user) { User.new }
@@ -18,19 +23,26 @@ describe "User can add vehicle to profile" do
       within '.year-menu' do
         find("option[value='2018']").select_option
       end
-      select "Chevrolet", from: "make-menu"
-      select "Volt", from: "Select Model"
-      click_on 'Find'
+      within '.make-menu' do
+        find("option[value='Chevrolet']").select_option
+      end
+      within '.model-menu' do
+        find("option[value='Bolt EV']").select_option
+      end
+      within '.option-menu' do
+        find("option[value='39786'").select_option
+      end
+      click_on 'Find!'
 
-      expect(current_path).to eq(search_path)
+      expect(current_path).to eq(new_search_path)
       expect(page).to have_content("Make: Chevrolet")
-      expect(page).to have_content("Model: Volt")
-      expect(page).to have_content("Year: 2014")
-      expect(page).to have_content("Fuel Type: Premium Gas or Electricity")
-      expect(page).to have_content("Range: 38 miles")
-      expect(page).to have_content("MPGe: 98")
+      expect(page).to have_content("Model: Bolt EV")
+      expect(page).to have_content("Year: 2018")
+      expect(page).to have_content("Fuel Type: Electricity")
+      expect(page).to have_content("Range: 238 miles")
+      expect(page).to have_content("MPGe: 0")
       expect(page).to have_content("Time to charge at 120V: n/a")
-      expect(page).to have_content("Time to charge at 240V: 4.0 hours")
+      expect(page).to have_content("Time to charge at 240V: 9 hours")
     end
   end
 end
