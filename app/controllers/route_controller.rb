01b9c8f1ba9c55b41fa72f93e6a52c663bbdca77
Ellen Cornelius <ecorneli@iwu.edu>
2018-04-19 12:17:16 -0600

    Add transform method.

--- app/controllers/route_controller.rb
+++ app/controllers/route_controller.rb
@@ -5,7 +5,23 @@ class RouteController < ApplicationController
 
   def show
     linestring = GeocodeService.new.get_linestring(params[:start_address], params[:end_address])
-    @stations = NrelService.new(linestring, params[:radius]).get_stations.to_h.to_json
+    stations = NrelService.new(linestring, params[:radius]).get_stations
+    @stations = transform(stations).to_json.html_safe
     @route = GeocodeService.new.get_route
+  end
+
+  def transform(stations)
+    stations.map do |station|
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": station
+          },
+        "properties": {
+          "icon": "monument"
+          }
+        }
+    end
   end
 end
